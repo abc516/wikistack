9a8e1e3f794c57a34131c2d21c004b46be4087f6
Keisuke Kido <kskkido@gmail.com>
2017-03-13 10:31:52 -0400

    added  getter to page model

--- models/index.js
+++ models/index.js
@@ -1,27 +1,37 @@
 var Sequelize = require('sequelize');
 var db = new Sequelize('postgres://localhost:5432/wikistack');
+// var db = new Sequelize('postgres://localhost:5432/wikistack', {
+//     logging: false
+// });
 
 var Page = db.define('page', {
     title: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING, 
+        allowNull: false,
     },
     urlTitle: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING, , allowNull: false, validate: {isUrl: true}
     },
     content: {
-        type: Sequelize.TEXT
+        type: Sequelize.TEXT, allowNull: false
     },
     status: {
         type: Sequelize.ENUM('open', 'closed')
+    },
+    date: {
+        type: Sequelize.DATE, defaultValue: Sequelize.Now
+    },
+    getterMethods: {
+        route: function () {return '/wiki/' + this.urlTitle}
     }
 });
 
 var User = db.define('user', {
     name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING, allowNull: false
     },
     email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING, allowNull: false, validate: {isEmail: true}
     }
 });
 
